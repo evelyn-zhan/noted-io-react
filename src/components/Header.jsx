0f8ea854424021d7c4b13b0965487ae5a6da1f84
Evelyn <evelynzanpx@gmail.com>
2024-05-29 16:03:32 +0700

    Fix: Header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className="header">
             <div className="header__logo">
-                <img src="/public/images/icon.png" alt="icon" width={40} height={40} />
+                <img src="/images/icon.png" alt="icon" width={40} height={40} />
                 <span>noted.io</span>
             </div>
             <p className="header__text">
